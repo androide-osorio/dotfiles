e6ec787672c138dc816fa8a4f984855a74b39a7c
Andres Osorio <androideosorio@me.com>
2022-05-15 23:25:30 +0200

    add fish config syncing

--- .fish/config.fish
+++ .fish/config.fish
@@ -9,3 +9,7 @@ set PATH $HOME/.cargo/bin $PATH
 
 # fnm setup
 fnm env | source
+
+# Setting PATH for Python 3.10
+# The original version is saved in /Users/androide.osorio/.config/fish/config.fish.pysave
+set -x PATH "/Library/Frameworks/Python.framework/Versions/3.10/bin" "$PATH"
